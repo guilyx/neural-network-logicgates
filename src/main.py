387e2d9a828e4f01c8a5ea35497ce2ec5a9d4889
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-21 18:25:53 +0100

    🚀 deploy or gate

--- src/main.py
+++ src/main.py
@@ -12,23 +12,44 @@ import sys
 from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
-def or_gate():
-    # Or Gate prediction
-    print("Training the OR gate model...")
+from lib.network import NeuralNetwork
 
-def and_gate():
+
+def or_gate(epochs, in1, in2):
+    # Or Gate prediction
+    print("You're using the OR gate Neural Network...")
+
+    t_inputs = np.array([[0, 0],
+                    [0, 1],
+                    [1, 0],
+                    [1, 1]])
+
+    t_outputs = np.array([[0, 1, 1, 1]]).T
+
+    or_predictor = NeuralNetwork()
+    print("Training...")
+    or_predictor.train(t_inputs, t_outputs, epochs)
+
+    print("Training finished, predicting your outputs : ")
+
+    predicted_output = or_predictor.predict(np.array([in1, in2]))
+    
+    print(np.array([in1, in2]), ' ---> ', predicted_output)
+    
+
+def and_gate(epochs, in1, in2):
     # And Gate prediction
     print("Training the AND gate model...")
 
-def xor_gate():
+def xor_gate(epochs, in1, in2):
     # Xor Gate prediction
     print("Training the XOR gate model...")
 
-def not_gate():
+def not_gate(epochs, in1, in2):
     # Not Gate prediction
     print("Training the NOT gate model...")
 
-def nand_gate():
+def nand_gate(epochs, in1, in2):
     # Nand Gate prediction
     print("Training the NAND gate model...")
 
@@ -41,3 +62,4 @@ if __name__ == "__main__":
     in2 = args['in2']
 
     ##### Start Training #####
+    or_gate(100, in1, in2)
