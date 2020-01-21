451c3b8724c06bf54415229ee63fb9aeda00e2db
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-22 00:35:48 +0100

    ✨ add gates

--- src/main.py
+++ src/main.py
@@ -14,44 +14,79 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
 from lib.network import NeuralNetwork
 
+def print_gate(gate):
+    print("You're using the " + gate.upper() + " Neural Network")
 
-def or_gate(epochs, in1, in2):
+def choose_gate(gate):
+    if gate == 'or' or gate == 'OR':
+        print_gate(gate)
+
+        t_inputs = np.array([[0, 0],
+                        [0, 1],
+                        [1, 0],
+                        [1, 1]])
+
+        t_outputs = np.array([[0, 1, 1, 1]]).T
+    
+    elif gate == 'xor' or gate == 'XOR':
+        print_gate(gate)
+
+        t_inputs = np.array([[0, 0],
+                        [0, 1],
+                        [1, 0],
+                        [1, 1]])
+
+        t_outputs = np.array([[0, 1, 1, 0]]).T
+    
+    elif gate == 'nor' or gate == 'NOR':
+        print_gate(gate)
+
+        t_inputs = np.array([[0, 0],
+                        [0, 1],
+                        [1, 0],
+                        [1, 1]])
+
+        t_outputs = np.array([[1, 0, 0, 0]]).T
+
+    elif gate == 'and' or gate == 'AND':
+        print_gate(gate)
+
+        t_inputs = np.array([[0, 0],
+                        [0, 1],
+                        [1, 0],
+                        [1, 1]])
+
+        t_outputs = np.array([[0, 0, 0, 1]]).T
+    
+    elif gate == 'nand' or gate == 'NAND':
+        print_gate(gate)
+
+        t_inputs = np.array([[0, 0],
+                        [0, 1],
+                        [1, 0],
+                        [1, 1]])
+
+        t_outputs = np.array([[1, 1, 1, 0]]).T
+
+    else:
+        print("Error : gate name not found !")
+
+    return t_inputs, t_outputs
+
+def predict_gate(epochs, in1, in2, gate):
     # Or Gate prediction
-    print("You're using the OR gate Neural Network...")
+    t_inputs, t_outputs = choose_gate(gate)
 
-    t_inputs = np.array([[0, 0],
-                    [0, 1],
-                    [1, 0],
-                    [1, 1]])
-
-    t_outputs = np.array([[0, 1, 1, 1]]).T
-
-    or_predictor = NeuralNetwork()
+    madame_irma = NeuralNetwork()
     print("Training...")
-    or_predictor.train(t_inputs, t_outputs, epochs)
+    madame_irma.train(t_inputs, t_outputs, epochs)
 
-    print("Training finished, predicting your outputs : ")
+    training_time = "{0:.2f}".format(madame_irma.training_time) # rounding to two digits only
+    print("Training finished in " + training_time + " seconds, predicting your output(s) : ")
 
-    predicted_output = or_predictor.predict(np.array([in1, in2]))
+    predicted_output = madame_irma.predict(np.array([in1, in2]))
     
     print(np.array([in1, in2]), ' ---> ', np.round(predicted_output))
-    
-
-def and_gate(epochs, in1, in2):
-    # And Gate prediction
-    print("Training the AND gate model...")
-
-def xor_gate(epochs, in1, in2):
-    # Xor Gate prediction
-    print("Training the XOR gate model...")
-
-def not_gate(epochs, in1, in2):
-    # Not Gate prediction
-    print("Training the NOT gate model...")
-
-def nand_gate(epochs, in1, in2):
-    # Nand Gate prediction
-    print("Training the NAND gate model...")
 
 if __name__ == "__main__":
     ##### Getting args ######
@@ -61,5 +96,7 @@ if __name__ == "__main__":
     in1 = args['in1']
     in2 = args['in2']
 
+    epoch = 10000
+
     ##### Start Training #####
-    or_gate(10000, in1, in2)
+    predict_gate(epoch, in1, in2, gate)
