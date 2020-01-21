49542302d37c9f5a920dd6dea767f6b927a32c81
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-21 18:18:57 +0100

    🎨 add epochs getter

--- lib/network.py
+++ lib/network.py
@@ -7,3 +7,49 @@ class NeuralNetwork():
             self.synapses = 2 * np.random.random((2, 1)) - 1
         else: 
             self.synapses = synapses
+
+        self.epoch = 0
+
+
+    def sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+
+    
+    def tanh(self, z):
+        return (2.0 / (1 + np.exp(-2*z))) - 1
+
+    
+    def tanh_prime(self, z):
+        return 1.0 - self.tanh(z)**2
+
+
+    def reshape(self, inputs):
+        if (inputs[0].shape != self.synapses.shape):
+            dim = inputs.shape
+            self.synapses.resize(dim[0], dim[1])
+        print("Synapses vector reshaped to the inputs' size..")
+
+    
+    def predict(self, inputs):
+        inputs = inputs.astype(float)
+        outputs = self.tanh(np.dot(inputs, self.synapses))
+        np.round(outputs)
+        return outputs
+
+    
+    def train(self, t_inputs, t_outputs, epochs):
+        for iterations in range(epochs):
+
+            self.epoch = iterations
+            
+            input_layer = t_inputs
+            outputs = self.tanh(np.dot(input_layer, self.synapses))
+            np.round(outputs)
+            error = t_outputs - outputs
+
+            adjustments = error * self.tanh_prime(outputs)
+            self.synapses += np.dot(input_layer.T, adjustments)
