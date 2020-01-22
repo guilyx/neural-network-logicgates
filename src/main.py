cfa5c82ec43a26639d817a4e43d69c809295ca15
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-22 02:35:01 +0100

    🎉 update main

--- src/main.py
+++ src/main.py
@@ -13,80 +13,7 @@ from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
 from lib.network import NeuralNetwork
-
-def print_gate(gate):
-    print("You're using the " + gate.upper() + " Neural Network")
-
-def choose_gate(gate):
-    if gate == 'or' or gate == 'OR':
-        print_gate(gate)
-
-        t_inputs = np.array([[0, 0],
-                        [0, 1],
-                        [1, 0],
-                        [1, 1]])
-
-        t_outputs = np.array([[0, 1, 1, 1]]).T
-    
-    elif gate == 'xor' or gate == 'XOR':
-        print_gate(gate)
-
-        t_inputs = np.array([[0, 0],
-                        [0, 1],
-                        [1, 0],
-                        [1, 1]])
-
-        t_outputs = np.array([[0, 1, 1, 0]]).T
-    
-    elif gate == 'nor' or gate == 'NOR':
-        print_gate(gate)
-
-        t_inputs = np.array([[0, 0],
-                        [0, 1],
-                        [1, 0],
-                        [1, 1]])
-
-        t_outputs = np.array([[1, 0, 0, 0]]).T
-
-    elif gate == 'and' or gate == 'AND':
-        print_gate(gate)
-
-        t_inputs = np.array([[0, 0],
-                        [0, 1],
-                        [1, 0],
-                        [1, 1]])
-
-        t_outputs = np.array([[0, 0, 0, 1]]).T
-    
-    elif gate == 'nand' or gate == 'NAND':
-        print_gate(gate)
-
-        t_inputs = np.array([[0, 0],
-                        [0, 1],
-                        [1, 0],
-                        [1, 1]])
-
-        t_outputs = np.array([[1, 1, 1, 0]]).T
-
-    else:
-        print("Error : gate name not found !")
-
-    return t_inputs, t_outputs
-
-def predict_gate(epochs, in1, in2, gate):
-    # Or Gate prediction
-    t_inputs, t_outputs = choose_gate(gate)
-
-    madame_irma = NeuralNetwork()
-    print("Training...")
-    madame_irma.train(t_inputs, t_outputs, epochs)
-
-    training_time = "{0:.2f}".format(madame_irma.training_time) # rounding to two digits only
-    print("Training finished in " + training_time + " seconds, predicting your output(s) : ")
-
-    predicted_output = madame_irma.predict(np.array([in1, in2]))
-    
-    print(np.array([in1, in2]), ' ---> ', np.round(predicted_output))
+from lib.gate_predictions import LogicGate
 
 if __name__ == "__main__":
     ##### Getting args ######
@@ -97,6 +24,10 @@ if __name__ == "__main__":
     in2 = args['in2']
 
     epoch = 10000
+    learning_rate = 1
 
     ##### Start Training #####
-    predict_gate(epoch, in1, in2, gate)
+    gate_ = LogicGate(gate, in1, in2)
+    pr_output = gate_.predict_gate(epoch, learning_rate)
+
+    print(pr_output)
