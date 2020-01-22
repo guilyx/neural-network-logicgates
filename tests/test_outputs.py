a5dcf3d83d93f80bde0a210f3ae7f93c1e8a4abf
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-22 02:22:38 +0100

    🎉 Init fixing branch

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -33,3 +33,102 @@ def test_outputs_or():
         pr_out = gate_.predict_gate(epochs, learning_rate)
 
         assert(pr_out[0] > expected_outputs[i][0] - error and pr_out[0] < expected_outputs[i][0] + error)
+
+def test_outputs_nor():
+    gate = 'nor'
+    epochs = randint(100, 100000)
+    learning_rate = 0.05
+    
+    # error = 10/float(epochs)
+    error = .1
+
+    expected_inputs = np.array([[0, 0],
+                         [0, 1],
+                         [1, 0],
+                         [1, 1]])
+
+    expected_outputs = np.array([[1, 0, 0, 0]]).T
+    
+    for i in range(3):
+        in1 = expected_inputs[i][0]
+        in2 = expected_inputs[i][1]
+
+        gate_ = LogicGate(gate, in1, in2)
+        pr_out = gate_.predict_gate(epochs, learning_rate)
+
+        assert(pr_out[0] > expected_outputs[i][0] - error and pr_out[0] < expected_outputs[i][0] + error)
+
+
+def test_outputs_xor():
+    gate = 'xor'
+    epochs = randint(100, 100000)
+    learning_rate = 0.05
+    
+    # error = 10/float(epochs)
+    error = .1
+
+    expected_inputs = np.array([[0, 0],
+                         [0, 1],
+                         [1, 0],
+                         [1, 1]])
+
+    expected_outputs = np.array([[0, 1, 1, 0]]).T
+    
+    for i in range(3):
+        in1 = expected_inputs[i][0]
+        in2 = expected_inputs[i][1]
+
+        gate_ = LogicGate(gate, in1, in2)
+        pr_out = gate_.predict_gate(epochs, learning_rate)
+
+        assert(pr_out[0] > expected_outputs[i][0] - error and pr_out[0] < expected_outputs[i][0] + error)
+
+
+def test_outputs_and():
+    gate = 'and'
+    epochs = randint(100, 100000)
+    learning_rate = 0.05
+    
+    # error = 10/float(epochs)
+    error = .1
+
+    expected_inputs = np.array([[0, 0],
+                         [0, 1],
+                         [1, 0],
+                         [1, 1]])
+
+    expected_outputs = np.array([[0, 0, 0, 1]]).T
+    
+    for i in range(3):
+        in1 = expected_inputs[i][0]
+        in2 = expected_inputs[i][1]
+
+        gate_ = LogicGate(gate, in1, in2)
+        pr_out = gate_.predict_gate(epochs, learning_rate)
+
+        assert(pr_out[0] > expected_outputs[i][0] - error and pr_out[0] < expected_outputs[i][0] + error)
+
+
+def test_outputs_nand():
+    gate = 'nand'
+    epochs = randint(100, 100000)
+    learning_rate = 0.05
+    
+    # error = 10/float(epochs)
+    error = .1
+
+    expected_inputs = np.array([[0, 0],
+                         [0, 1],
+                         [1, 0],
+                         [1, 1]])
+
+    expected_outputs = np.array([[1, 1, 1, 0]]).T
+    
+    for i in range(3):
+        in1 = expected_inputs[i][0]
+        in2 = expected_inputs[i][1]
+
+        gate_ = LogicGate(gate, in1, in2)
+        pr_out = gate_.predict_gate(epochs, learning_rate)
+
+        assert(pr_out[0] > expected_outputs[i][0] - error and pr_out[0] < expected_outputs[i][0] + error)
